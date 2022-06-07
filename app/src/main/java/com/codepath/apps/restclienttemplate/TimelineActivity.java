1fa9522a9ccf8a5f47bb39be211b5b99a3bd56fa
Elmer Adrián Ortega Valdés <elmeradrianv@fb.com>
2022-06-07 07:26:04 -0700

    Step 5: Users can logout

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
@@ -24,6 +27,10 @@ public class TimelineActivity extends AppCompatActivity {
     RecyclerView rvTweets;
     List<Tweet> tweets;
     TweetsAdapter adapter;
+    Button btnLogout;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,9 +45,28 @@ public class TimelineActivity extends AppCompatActivity {
         rvTweets.setLayoutManager(new LinearLayoutManager(this));
         rvTweets.setAdapter(adapter);
 
+        btnLogout=findViewById(R.id.btnLogout);
+
         //recycler view setup: layout manager and the adapter
         populateHomeTimeline();
-        
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                onLogoutButton(v);
+            }
+
+            void onLogoutButton(View view) {
+                // forget who's logged in
+                TwitterApp.getRestClient(TimelineActivity.this).clearAccessToken();
+
+                // navigate backwards to Login screen
+                Intent i = new Intent(TimelineActivity.this, LoginActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // same as above
+                startActivity(i);
+            }
+        });
     }
 
     private void populateHomeTimeline() {
@@ -70,4 +96,8 @@ public class TimelineActivity extends AppCompatActivity {
             }
         });
     }
+
+
+
+
 }
