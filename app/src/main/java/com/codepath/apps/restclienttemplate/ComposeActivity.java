4cd3217c397ebec8b0747631d73cdc3f7b29695e
Elmer Adrián Ortega Valdés <elmeradrianv@fb.com>
2022-06-07 10:48:10 -0700

    Step 8: Publish the Tweet and insert into the Tweets list

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -2,15 +2,29 @@ package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.parceler.Parcels;
+
+import javax.security.auth.login.LoginException;
+
+import okhttp3.Headers;
+
 public class ComposeActivity extends AppCompatActivity {
     EditText etCompose;
     Button btnTweet;
+    TwitterClient client;
+    public static final String TAG="ComposeActivity";
     public static final int MAX_TWEET_LENGTH=140;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +32,7 @@ public class ComposeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_compose);
         etCompose=findViewById(R.id.etCompose);
         btnTweet=findViewById(R.id.btnTweet);
+        client = TwitterApp.getRestClient(this);
 
         // Set click listener on button
         btnTweet.setOnClickListener(new View.OnClickListener(){
@@ -33,6 +48,28 @@ public class ComposeActivity extends AppCompatActivity {
                     return;
                 }
                 Toast.makeText(ComposeActivity.this, tweetContent, Toast.LENGTH_LONG).show();
+                client.publishTweet(tweetContent, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                        Log.i(TAG, "onSuccess to publish tweet ");
+                        try {
+                            Tweet tweet = Tweet.fromJson(json.jsonObject);
+                            Log.i(TAG, "Published tweet says:"+tweet.body);
+                            Intent intent = new Intent();
+                            intent.putExtra("tweet", Parcels.wrap(tweet));
+                            setResult(RESULT_OK,intent);
+                            //Close the activity, pass data to parent
+                            finish();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                        Log.e(TAG, "onFailure: to publish tweet",throwable );
+                    }
+                });
             }
         });
 
--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -18,15 +18,19 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.annotation.Nullable;
 
 import okhttp3.Headers;
 
 public class TimelineActivity extends AppCompatActivity {
     TwitterClient client;
-    String TAG= "TimelineActivity.java";
+    public static final String TAG= "TimelineActivity.java";
+    private final int REQUEST_CODE=20;
     RecyclerView rvTweets;
     List<Tweet> tweets;
     TweetsAdapter adapter;
@@ -82,10 +86,26 @@ public class TimelineActivity extends AppCompatActivity {
             //Compose icon has been selected
             //Toast.makeText(this, "Compose!", Toast.LENGTH_SHORT).show();
             Intent intent = new Intent(this, ComposeActivity.class);
-            startActivity(intent);
+
+            startActivityForResult(intent,REQUEST_CODE);
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        if(requestCode==REQUEST_CODE && resultCode==RESULT_OK){
+            //Get data from the intent (tweet)
+            Tweet tweet =(Tweet) Parcels.unwrap(data.getParcelableExtra("tweet"));
+            // Update the RV with the tweet
+            //Modify data source of tweets
+            tweets.add(0,tweet);
+            //Update the adapater
+           adapter.notifyItemInserted(0);
+           //To return to position 0
+           rvTweets.smoothScrollToPosition(0);
+        }
+        super.onActivityResult(requestCode,resultCode,data);
     }
 
     private void populateHomeTimeline() {
